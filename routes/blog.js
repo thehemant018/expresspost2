9e6c8cd067d2096ed61e58a0f5dac02a5ad313dc
Hemant Maurya <113825684+thehemant018@users.noreply.github.com>
2024-01-22 09:23:10 +0530

    eighteen commit

--- routes/blog.js
+++ routes/blog.js
@@ -17,7 +17,10 @@ const storage = multer.diskStorage({
     }
   })
   
-  const upload = multer({ storage: storage })
+  const upload = multer({ storage: storage,limits: {
+    fileSize: 5 * 1024 * 1024,
+    time:300
+}, })
   
 
 
